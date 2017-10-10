677e25a579d3652cc1af67d721da11c3c1a9655a
seledkinpylesos <32525647+seledkinpylesos@users.noreply.github.com>
2017-10-10 17:45:26 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,39 +1,40 @@
 #!/bin/bash -ex
 
-if [[ ! $(grep -q dtoverlay=w1-gpio /boot/config.txt)]]
+if [[ ! $(grep -q dtoverlay=w1-gpio /boot/config.txt) ]]
 then 
 	echo "dtoverlay=w1-gpio" >> /boot/config.txt
 fi
 
-apt-get update
-apt-get upgrade
+apt-get -y update
+apt-get -y upgrade
 
 # Install necessary software
 apt-get install -y git python3-w1thermsensor python3-pigpio
 pip install awscli --upgrade
 pip install boto3
 
-# Set aliases and AWS creds 
-cat <<EOF > /home/pi/.bashrc
+# Set AWS creds
+mkdir $HOME/.aws
+cat <<EOF > $HOME/.aws/credentials
+AWS_ACCESS_KEY_ID='$1'
+AWS_SECRET_ACCESS_KEY='$2'
+AWS_DEFAULT_REGION='us-east-2'
+EOF
 
-export AWS_ACCESS_KEY_ID='$1'
-export AWS_SECRET_ACCESS_KEY='$2'
-export AWS_DEFAULT_REGION='us-east-2'
+# Set aliases for pi
+cat <<EOF >> /home/pi/.bashrc
 alias ll='ls -alFG'
+alias python=python3
 
 EOF
 
-# Copy .bashrc file to the root user
+# Copy .bashrc file to the root user for fancy prompt and aliases
 cp /home/pi/.bashrc $HOME/.bashrc
 
-
 # Install ssh keys
-export $HOME/.bashrc
-
 [[ ! -d $HOME/.ssh ]] && mkdir $HOME/.ssh && chmod 700 $HOME/.ssh
 [[ ! -e $HOME/.ssh/authorized_keys ]] && aws s3 cp s3://us-east-2-rpi-creds/id_rsa.pub $HOME/.ssh/authorized_keys
 [[ ! -e $HOME/.ssh/id_rsa ]] && aws s3 cp s3://us-east-2-rpi-creds/id_rsa $HOME/.ssh/id_rsa
-
 chmod 600 $HOME/.ssh/authorized_keys
 chmod 600 $HOME/.ssh/id_rsa
 
