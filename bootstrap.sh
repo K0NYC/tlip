a650d81d70de23030b5642dc6dff165fcb2b6f7c
Ron Levit <rlevit@amplify.com>
2017-10-09 22:45:35 -0400

    more updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,8 +9,8 @@ apt-get update
 apt-get upgrade
 
 # Install necessary software
-apt-get install -y git awscli python3-w1thermsensor python3-pigpio
-
+apt-get install -y git python3-w1thermsensor python3-pigpio
+pip install awscli --upgrade
 pip install boto3
 
 # Set aliases and AWS creds 
@@ -38,7 +38,13 @@ chmod 600 $HOME/.ssh/authorized_keys
 chmod 600 $HOME/.ssh/id_rsa
 
 # Set up git repo
+cat <<EOF > $HOME/.ssh/known_hosts
+github.com,192.30.253.113 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
+EOF
 
+mkdir -p /opt/gits
+cd /opt/gits
+git clone git@github.com:seledkinpylesos/rpi.git
 
 
 
