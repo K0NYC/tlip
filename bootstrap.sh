b21f37c17fff44c21b872d5a26f50c488d560f23
seledkinpylesos <32525647+seledkinpylesos@users.noreply.github.com>
2017-10-13 09:07:47 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,7 +18,7 @@ then
 [default]
 AWS_ACCESS_KEY_ID=$1
 AWS_SECRET_ACCESS_KEY=$2
-AWS_DEFAULT_REGION=us-east-2
+AWS_DEFAULT_REGION=us-east-1
 EOF
 fi
 
@@ -26,7 +26,7 @@ if [[ ! -e $HOME/.aws/config ]]
 then
 	cat <<EOF > $HOME/.aws/config
 [default]
-region = us-east-2
+region = us-east-1
 EOF
 fi
 
@@ -44,8 +44,8 @@ cp /home/pi/.bashrc $HOME/.bashrc
 
 # Install ssh keys
 [[ ! -d $HOME/.ssh ]] && mkdir $HOME/.ssh && chmod 700 $HOME/.ssh
-[[ ! -e $HOME/.ssh/authorized_keys ]] && aws s3 cp s3://us-east-2-rpi-creds/id_rsa.pub $HOME/.ssh/authorized_keys
-[[ ! -e $HOME/.ssh/id_rsa ]] && aws s3 cp s3://us-east-2-rpi-creds/id_rsa $HOME/.ssh/id_rsa
+[[ ! -e $HOME/.ssh/authorized_keys ]] && aws s3 cp s3://us-east-1-rpi-creds/id_rsa.pub $HOME/.ssh/authorized_keys
+[[ ! -e $HOME/.ssh/id_rsa ]] && aws s3 cp s3://us-east-1-rpi-creds/id_rsa $HOME/.ssh/id_rsa
 chmod 600 $HOME/.ssh/authorized_keys
 chmod 600 $HOME/.ssh/id_rsa
 
